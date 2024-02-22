e98e7966b8940bb9fd59f7dcf662ebf2b46afead
Antonio Maroto Blasco <antoniomarotoblasco@gmail.com>
2024-02-22 17:06:40 +0100

    First commit

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Resources\ArticleCollection;
+use App\Http\Resources\ArticleResource;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    // Obetener un artículo
+    public function show(Article $article): ArticleResource
+    {
+        return ArticleResource::make($article);
+    }
+
+    // Obtener todos los artículos
+    public function index(): ArticleCollection
+    {
+        return ArticleCollection::make(Article::all());
+    }
+
+    // Crear un articulo
+    public function create(Request $request)
+    {
+        $article = Article::create([
+            'title' => $request->input('data.attributes.title'),
+            'slug' => $request->input('data.attributes.slug'),
+            'content' => $request->input('data.attributes.content'),
+        ]);
+
+        return ArticleResource::make($article);
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\ArticleController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::get('articles', [ArticleController::class, 'index'])->name('api.v1.articles.index');
+
+Route::get('article/{article}', [ArticleController::class, 'show'])->name('api.v1.articles.show');
+
+Route::post('articles', [ArticleController::class, 'create'])->name('api.v1.articles.create');
+
+
