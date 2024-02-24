e594937367ba20b3d007e018d49fb8267ae46986
Antonio Maroto Blasco <antoniomarotoblasco@gmail.com>
2024-02-24 13:00:48 +0100

    Refactorizado Sort articles

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -7,30 +7,14 @@ use App\Http\Resources\ArticleCollection;
 use App\Http\Resources\ArticleResource;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Response;
-use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 use App\Models\Article;
 
 
 class ArticleController extends Controller
 {
-    public function index(Request $request): ArticleCollection
+    public function index(): ArticleCollection
     {
-
-        $articles = Article::query();
-
-        if ($request->filled('sort')) {
-            $sortFields = explode(',', $request->input('sort'));
-
-            $allowedSorts = ['title', 'content'];
-
-            foreach ($sortFields as $sortField) {
-                $sortDirection = Str::of($sortField)->startsWith('-') ? 'desc' : 'asc';
-                $sortField = ltrim($sortField, '-');
-                abort_unless(in_array($sortField, $allowedSorts), 400);
-                $articles->orderBy($sortField, $sortDirection);
-            }
-        }
+        $articles = Article::allowedSorts(['title', 'content']);
         return ArticleCollection::make($articles->get());
     }
 
